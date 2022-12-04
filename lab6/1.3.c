53a78d00523a8f241932368fdece17779d9b5d1c
alexandrakhraltsova <91497049+alexandrakhraltsova@users.noreply.github.com>
2022-12-04 22:50:09 +0300

    Create 1.3.c

---
+++ lab6/1.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+extern char **environ;
+int main(int argc, char *argv[])
+{
+    char **p;
+    int count = 0;
+    const int MAX = 10;
+    for(p = environ; *p != NULL && count < MAX; p++, count++)
+      printf("%s\n", *p);
+    return 0;
+}
