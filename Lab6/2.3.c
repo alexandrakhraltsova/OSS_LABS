aaadb90e4a786cea8557824a4c1faba62bdf611b
alexandrakhraltsova <91497049+alexandrakhraltsova@users.noreply.github.com>
2022-12-04 22:38:19 +0300

    Update 2.3.c

--- Lab6/2.3.c
+++ Lab6/2.3.c
@@ -6,10 +6,11 @@ int main(void)
   int pid = fork();
   
   for (int i = 0; i < 10; i++) {
-  	  if (pid > 0) {
-		      fork();
-	    }
+  	if (pid > 0) {
+  		fork();
+  	}
   }
+	
   sleep(3);
   return 0;
 }
