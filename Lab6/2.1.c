d6107bfe3ba708527cc0c19e682f1b5630bc39bb
alexandrakhraltsova <91497049+alexandrakhraltsova@users.noreply.github.com>
2022-12-04 22:33:33 +0300

    Create 2.1.c

---
+++ Lab6/2.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int pid = fork();
+  if (pid == 0) {
+    printf("Это сообщение из дочернего процесса\nДочерний PID = %d\nРодительский PID = %d\n", pid, getppid());
+  }
+  return 0;
+}
