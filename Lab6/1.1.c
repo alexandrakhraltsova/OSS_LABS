d08dbccddb21f8e22cbc8724c6d82ffabc6a6b1d
alexandrakhraltsova <91497049+alexandrakhraltsova@users.noreply.github.com>
2022-12-04 20:25:29 +0300

    Update 1.1.c

--- Lab6/1.1.c
+++ Lab6/1.1.c
@@ -3,10 +3,10 @@
 extern char **environ;
 int main(int argc, char *argv[])
 {
-  char **p;
-  int count = 0;
-  for(p = environ; *p != NULL; p++)
-    count++;
-  printf("Number of environment variables: %d\n", count);
-  return 0;
+    char **p;
+    int count = 0;
+    for(p = environ; *p != NULL; p++)
+      count++;
+    printf("Number of environment variables: %d\n", count);
+    return 0;
 }
