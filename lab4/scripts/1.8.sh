ff11bea1fa5c2a6cd798e15f7713f7a1a04ea837
alexandrakhraltsova <91497049+alexandrakhraltsova@users.noreply.github.com>
2022-12-24 09:59:56 +0300

    Количество процессов

--- lab4/scripts/1.8.sh
+++ lab4/scripts/1.8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "Процессов пользователя:\n$USER"
-ps -f -u $USER | wc -l
-echo "Процессов пользователя root:"
-ps -f -u root |wc -l
+echo "Процессов пользователя $(whoami);:";
+ps -U "$(whoami)" | wc -l;
+echo "Процессов пользователя root:";
+ps -U root | wc -l;
